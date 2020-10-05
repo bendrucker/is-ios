d4acbb7dc0bd76dbaa613f649361547742aa0ad3
Simon B <simon.bohlin@gmail.com>
2020-10-05 22:38:13 +0200

    Enable detecting iOS 13 on iPad (#3)

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 var navigator = require('global/window').navigator
 
-module.exports = (function detect_iOS (userAgent) {
-  return /iPad|iPhone|iPod/.test(userAgent)
-})(navigator ? navigator.userAgent : '')
+module.exports = (function detect_iOS (navigator) {
+  return (
+    /iPad|iPhone|iPod/.test(navigator.userAgent || '') ||
+    (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1) // iPad iOS 13
+  )
+})(navigator || {})
