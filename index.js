cd858f3508947fc9489c12395f8d430d4215f069
Ben Drucker <bvdrucker@gmail.com>
2020-10-05 13:48:13 -0700

    camel case

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var navigator = require('global/window').navigator
 
-module.exports = (function detect_iOS (navigator) {
+module.exports = (function detectiOS (navigator) {
   return (
     /iPad|iPhone|iPod/.test(navigator.userAgent || '') ||
     (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1) // iPad iOS 13
