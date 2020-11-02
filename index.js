b627e2191da0a4a5a2ad4832c54e657c1c484dc9
Ben Drucker <bvdrucker@gmail.com>
2020-11-02 11:58:40 -0800

    standard --fix

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 var navigator = require('global/window').navigator
 
-module.exports = typeof navigator !== "undefined" &&
+module.exports = typeof navigator !== 'undefined' &&
   (/iPad|iPhone|iPod/.test(navigator.userAgent || '') ||
   (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)) // iPad iOS 13
