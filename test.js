7de0f08eb171901ef3a74ae919893f3ed8a1ee39
Richie Bendall <richiebendall@gmail.com>
2020-11-03 08:25:00 +1300

    Update unit tests

--- test.js
+++ test.js
@@ -4,13 +4,13 @@ var test = require('tape')
 var proxyquire = require('proxyquire')
 
 test(function (t) {
-  t.notOk(require('./'))
-  t.ok(proxyquire('./', {
+  t.equal(require('./'), false)
+  t.equal(proxyquire('./', {
     'global/window': {
       navigator: {
         userAgent: 'Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.10'
       }
     }
-  }))
+  }), true)
   t.end()
 })
