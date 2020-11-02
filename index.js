52f433673058476368d1483d61cc823180e17566
Richie Bendall <richiebendall@gmail.com>
2020-11-03 08:57:37 +1300

    Simplify implementation (#5)

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 
 var navigator = require('global/window').navigator
 
-module.exports = (function detectiOS (navigator) {
-  return (
-    /iPad|iPhone|iPod/.test(navigator.userAgent || '') ||
-    (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1) // iPad iOS 13
-  )
-})(navigator || {})
+module.exports = typeof navigator !== "undefined" &&
+  (/iPad|iPhone|iPod/.test(navigator.userAgent || '') ||
+  (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)) // iPad iOS 13
