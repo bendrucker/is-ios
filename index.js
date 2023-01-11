94298b3c2eb56e46d37bd10808b888af45443fc2
roeib <berkovichroei@gmail.com>
2023-01-11 09:50:22 +0200

    better checking mac and global regex for userAgent
    better checking mac and global regex for userAgent

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 var navigator = require('global/window').navigator
 
 module.exports = typeof navigator !== 'undefined' &&
-  (/iPad|iPhone|iPod/.test(navigator.userAgent || '') ||
-  (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)) // iPad iOS 13
+ (/iphone|ipad|ipod/i).test(window.navigator.userAgent.toLowerCase()) || 
+  (navigator.platform.toLowerCase().startsWith("mac") && navigator.maxTouchPoints > 1)) // iPad iOS 13
